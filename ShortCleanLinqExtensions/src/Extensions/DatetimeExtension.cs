8e2a6ecab9a27f9c08ffd8395590b19d1d836b06
Gabriel Sales <ghe_sales@hotmail.com>
2024-06-18 23:48:40 -0300

    Add Method diff to get difference two dates

--- ShortCleanLinqExtensions/src/Extensions/DatetimeExtension.cs
+++ ShortCleanLinqExtensions/src/Extensions/DatetimeExtension.cs
@@ -6,5 +6,19 @@
         {
             return type == Support.DateTime.Type.Utc ? DateTime.UtcNow : DateTime.Now;
         }
+        /// <summary>
+        /// Create diffences two datetimes and return timespan
+        /// You can get diffence.Days
+        /// You can get diffence.Hours
+        /// You can get diffence.Minutes
+        /// You can get diffence.Seconds
+        /// You can get diffence.Milliseconds
+        /// You can get diffence.Ticks
+        /// </summary>
+        /// <param name="datetime"></param>
+        /// <param name="anotherDateTime"></param>
+        /// <returns></returns>
+        public static TimeSpan Diff(this DateTime datetime, DateTime anotherDateTime) => datetime - anotherDateTime;
+
     }
 }
